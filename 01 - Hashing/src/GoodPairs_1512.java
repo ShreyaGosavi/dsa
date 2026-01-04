8748a7ad099b2443a80227ba408cc293a3d92de4
Shreya_Gosavi <shreya.p.gosavi@gmail.com>
2026-01-04 22:11:21 +0530

    day 4 commit

--- 01 - Hashing/src/GoodPairs_1512.java
+++ 01 - Hashing/src/GoodPairs_1512.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class GoodPairs_1512 {
+import java.util.HashMap;
+
+public class GoodPairs_1512 {
+    public static void main(String[] args){
+        int[] nums = {1, 2, 3, 1, 1, 3};
+
+        int result = numIdenticalPairs(nums);
+        System.out.println("Number of good pairs: " + result);
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            count += freq.getOrDefault(num, 0);
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
 }

406c9cee1aaaee91d32d45735707cf54da33e5a0
Shreya_Gosavi <shreya.p.gosavi@gmail.com>
2026-01-28 19:58:19 +0530

    day 28 commit

---
+++ 02 - Trees/src/WordLadder_127.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class WordLadder_127 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord))
+            return 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                char[] arr = word.toCharArray();
+
+                for (int j = 0; j < arr.length; j++) {
+                    char original = arr[j];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original)
+                            continue;
+
+                        arr[j] = c;
+                        String next = new String(arr);
+
+                        if (next.equals(endWord)) {
+                            return level + 1;
+                        }
+
+                        if (dict.contains(next)) {
+                            queue.offer(next);
+                            dict.remove(next);
+                        }
+                    }
+                    arr[j] = original;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}

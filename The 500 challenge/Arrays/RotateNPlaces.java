84ba797f9bfc2034f1e361c697d8bf4c7aca81c2
Shreya_Gosavi <shreya.p.gosavi@gmail.com>
2026-04-14 21:23:40 +0530

    fix: fixed errors

--- The 500 challenge/Arrays/RotateNPlaces.java
+++ The 500 challenge/Arrays/RotateNPlaces.java
@@ -1,36 +1,21 @@
-import static java.util.Collections.reverse;
-
 public class RotateNPlaces {
+        public void rotate(int[] arr, int k) {
+            int n = arr.length;
+            k = k % n;
 
-    public static int[] reverse(int[] arr){
-        int start = 0;
-        int end = arr.length - 1;
-
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+            reverse(arr, 0, n - 1);
+            reverse(arr, 0, k - 1);
+            reverse(arr, k, n - 1);
         }
-        return arr;
-    }
-    public static int[] reverse1(int[] arr, int start, int end){
-        while(start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+
+        public void reverse(int[] arr, int start, int end){
+            while(start < end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
         }
-        return arr;
-    }
-    public static int[] rotate(int[] arr, int n){
-        arr = reverse(arr);
 
-        arr = reverse1(arr, 0, arr.length - 1 - n);
-        reverse1(arr, arr.length - n, arr.length - 1);
-
-        return arr;
-    }
 }
